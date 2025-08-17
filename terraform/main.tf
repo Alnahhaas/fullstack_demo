23d7d07275963a1a770a8e3581709ec1f06af0ee
Hassan Alnahhaas <hassanalnahhaas@gmail.com>
2025-08-17 07:57:51 +0300

    apply terrafrom to  build images , run helm charts

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      # version = "v3.6"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      # version = "v3.0"
+    }
+  }
+}
+
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
@@ -8,9 +24,60 @@ provider "helm" {
   }
 }
 
+provider "docker" {}
+
+
+# ----------------------------
+# Docker Images
+# ----------------------------
+
+resource "docker_image" "backend" {
+  name = "alnahhaas/fullstack_demo_backend:latest"
+
+  build {
+    context    = "../backend"
+    dockerfile = "Dockerfile"
+  }
+
+  keep_locally = false  # push automatically to Docker Hub
+}
+
+resource "docker_image" "frontend" {
+  name = "alnahhaas/fullstack_demo_frontend:latest"
+
+  build {
+    context    = "../frontend"
+    dockerfile = "Dockerfile"
+  }
+
+  keep_locally = false
+}
+
+# ----------------------------
+# Helm Release
+# ----------------------------
+
 resource "helm_release" "fullstack_demo" {
-  name       = "sonnen1st"
-  chart      = "../helm-chart" 
+  name       = "fullstack-demo"
+  chart      = "../helm-chart"   # path to your local Helm chart
   namespace  = "default"
 
+  set = [
+    {
+      name  = "frontend.image"
+      value = docker_image.frontend.name
+    },
+    {
+      name  = "backend.image"
+      value = docker_image.backend.name
+    },
+    {
+      name  = "ingress.enabled"
+      value = true
+    },
+    {
+      name  = "ingress.host"
+      value = "myapp.local"
+    }
+  ]
 }
